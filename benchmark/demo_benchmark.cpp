75f3939ebf19c487a1d78dc7a104abb9c7673d06
Ramil_Gatin9 <nais.gatin@yandex.ru>
2021-04-29 16:38:05 +0300

    benchmark bug fix

--- benchmark/demo_benchmark.cpp
+++ benchmark/demo_benchmark.cpp
@@ -1,74 +1,141 @@
-#include <fstream>      // ifstream
-#include <iostream>     // cout
-#include <string>       // string, stoi
-#include <string_view>  // string_view
-#include <chrono>       // high_resolution_clock, duration_cast, nanoseconds
-#include <sstream>      // stringstream
-
-// подключаем вашу структуру данных
+#include <iostream>
 #include "data_structure.hpp"
+#include <ctime>
+#include <chrono>
+#include <fstream>
+#include "iostream"
+#include <string>
+#include <algorithm>
+#include <sstream>
+#include "vector"
 
 using namespace std;
-using namespace itis;
 
-// абсолютный путь до набора данных и папки проекта
-static constexpr auto kDatasetPath = string_view{PROJECT_DATASET_DIR};
-static constexpr auto kProjectPath = string_view{PROJECT_SOURCE_DIR};
 
-int main(int argc, char **argv) {
+vector<int> split(const string& s, char delimiter) {
+  vector<int> tokens;
+  string token;
+  istringstream tokenStream(s);
+  while (getline(tokenStream, token, delimiter)) {
+    tokens.push_back(stoi(token));
+  }
+  return tokens;
+}
 
-  // Tip 1: входные аргументы позволяют более гибко контролировать параметры вашей программы
 
-  // Можете передать путь до входного/выходного файла в качестве аргумента,
-  // т.е. не обязательно использовать kDatasetPath и прочие константы
+int main() {
+  cout<<"ss"<<endl;
+  //
+  ////ИЗМЕНИТЬ ПУТЬ К НАБОРАМ ДАННЫХ
+  //
+  //
+  //
+  //
+  string path = "C:\\Repos\\12.csv";
+  //
+  ////ИЗМЕНИТЬ ПУТЬ К НАБОРАМ ДАННЫХ
+  //
+  //
+  //
+  //
 
-  for (int index = 0; index < argc; index++) {
-    cout << "Arg: " << argv[index] << '\n';
+  ifstream fin;
+  fin.open(path);
+
+  string line;
+  vector<int> intValues;
+  if (!fin.is_open()) {
+    cout << "Error" << endl;
+
+  } else {
+    cout << "Success" << endl;
+
+    ////заполнение массива чисел значениями из .csv файла
+    while (getline(fin, line)) {
+
+      intValues = split(line, ',');
+    }
+
+    vector<itis::Node> NodeValues;
+    cout << intValues.size() << endl;
+
+    ////создание вершин декартового дерева из массива чисел
+
+    ////ДЛЯ КАЖДОГО НАБОРА ЧИСЕЛ МЕНЯТЬ РАНДОМАЙЗЕР
+    //// для 100 элементов  -- rand() % 99 + 1)
+    //// для 500 элементов  -- rand() % 499 + 1)
+    //// для 1000 элементов  -- rand() % 999 + 1)
+    //// ............................................
+    //// для 5 000 000 элементов -- rand() % 4 999 999 + 1
+    for (unsigned long i = 0; i < intValues.size() - 2; ++i) {
+
+      NodeValues.push_back(*new itis::Node(intValues[static_cast<unsigned long>(rand() % 99 + 1)],
+                                           intValues[static_cast<unsigned long>(rand() % 99 + 1)], nullptr,
+                                           nullptr));
+    }
+
+    ////создание деревьев
+    auto *cartesianTree = new itis::CartesianTree;
+
+
+//    auto *cartesianTreeA = new itis::CartesianTree; Пустое дерево для операции split. Убрать слеши при использовании split!!!
+//    auto *cartesianTreeB = new itis::CartesianTree; Пустое дерево для операции split  Убрать слеши при использовании split!!!
+
+    //// cartesianTree->split(cartesianTree->root_, rand() % (смотреть строку 63)+ 1, cartesianTreeA->root_, cartesianTreeB->root_);
+    /// Разделение дерева на два для операции merge
+
+
+
+    ////ЗАПОЛНЕНИЕ ДЕРЕВА ВЕРШИНАМИ
+    for (itis::Node node : NodeValues) {
+      cartesianTree->Insert(node.key, node.prior);
+    }
+
+
+
+    /// НАЧАЛО ЗАМЕРА ВРЕМЕНИ
+    const auto time_point_before = std::chrono::steady_clock::now();
+
+
+
+    ////Замерять  операцию здесь
+
+    ////cartesianTree->erase(cartesianTree->root_,intValues[static_cast<unsigned long>(rand() % (смотреть строку 63) + 1)]);
+    /// Операция удаления
+
+
+
+    //// cartesianTree->merge(cartesianTreeA->root_,cartesianTreeB->root_);
+    /// Операция слияния(ПЕРЕД ЭТИМ РАЗДЕЛИТЬ ДЕРЕВО НА ДВА С ПОМОЩЬЮ SPLIT(Строка 83)
+
+
+
+    ////cartesianTree->split(cartesianTree->root_, rand() % (смотреть строку 63)+ 1, cartesianTreeA->root_, cartesianTreeB->root_);
+    ////Операция разделения
+
+
+
+    //// cartesianTree->Search((rand() % (смотреть строку 63) + 1));
+    ////Операция поиска вершины
+
+
+
+
+
+    const auto time_point_after = std::chrono::steady_clock::now();
+    ////КОНЕЦ ЗАМЕРА ВРЕМЕНИ
+
+
+    const auto time_diff = time_point_after - time_point_before;
+
+
+    const  long long time_elapsed_ns = chrono::duration_cast<chrono::nanoseconds>(time_diff).count();
+
+
+    cout << "Time elapsed (ns): " << time_elapsed_ns << '\n';
+
   }
 
-  // Tip 2: для перевода строки в число можете использовать функцию stoi (string to integer)
-
-  // можете использовать функционал класса stringstream для обработки строки
-  auto ss = stringstream("0 1 2");  // передаете строку (входной аргумент или строку из файла) и обрабатываете ее
-
-  int number = 0;
-  ss >> number;  // number = 0
-  ss >> number;  // number = 1
-  ss >> number;  // number = 2
-
-  // работа с набором данных
-  const auto path = string(kDatasetPath);
-  cout << "Path to the 'dataset/' folder: " << path << endl;
-
-  auto input_file = ifstream(path + "/dataset-example.csv");
-
-  if (input_file) {
-    // чтение и обработка набора данных ...
-  }
-
-  // Контрольный тест: операции добавления, удаления, поиска и пр. над структурой данных
-
-  // Tip 3: время работы программы (или участка кода) можно осуществить
-  // как изнутри программы (std::chrono), так и сторонними утилитами
-
-  const auto time_point_before = chrono::high_resolution_clock::now();
-
-
-  int k=0;
-  for (int i=0; i<1000000;i++){
-    k=k+1;
-  }
-cout<<k<<endl;
-
-  // здесь находится участок кода, время которого необходимо замерить
-
-  const auto time_point_after = chrono::high_resolution_clock::now();
-
-  // переводим время в наносекунды
-  const auto time_diff = time_point_after - time_point_before;
-  const long time_elapsed_ns = chrono::duration_cast<chrono::nanoseconds>(time_diff).count();
-
-  cout << "Time elapsed (ns): " << time_elapsed_ns << '\n';
-
+  fin.close();
   return 0;
 }
